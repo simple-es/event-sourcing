eff8a0e81c0be90f2d9785a22983709f8f341305
Jasper N. Brouwer <jasper@nerdsweide.nl>
2015-03-14 16:09:14 +0100

    Have EventId use IdentifyingCapabilities

--- src/Event/Stream/EventId.php
+++ src/Event/Stream/EventId.php
@@ -6,48 +6,14 @@
 
 namespace SimpleES\EventSourcing\Event\Stream;
 
+use SimpleES\EventSourcing\Identifier\Identifies;
+use SimpleES\EventSourcing\Identifier\IdentifyingCapabilities;
+
 /**
  * @copyright Copyright (c) 2015 Future500 B.V.
  * @author    Jasper N. Brouwer <jasper@future500.nl>
  */
-final class EventId
+final class EventId implements Identifies
 {
-    /**
-     * @var string
-     */
-    private $id;
-
-    /**
-     * @param string $string
-     * @return EventId
-     */
-    public static function fromString($string)
-    {
-        return new EventId($string);
-    }
-
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param EventId $other
-     * @return bool
-     */
-    public function equals(EventId $other)
-    {
-        return ($other->id === $this->id);
-    }
-
-    /**
-     * @param string $id
-     */
-    private function __construct($id)
-    {
-        $this->id = (string) $id;
-    }
+    use IdentifyingCapabilities;
 }
